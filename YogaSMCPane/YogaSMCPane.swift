eca8afbf52c108b5042661faabcd3f9ccdab486f
Zhen <66577170+zhen-zen@users.noreply.github.com>
2020-09-23 00:33:56 -0700

    YogaSMCPane: support generic configuration

--- YogaSMCPane/YogaSMCPane.swift
+++ YogaSMCPane/YogaSMCPane.swift
@@ -11,16 +11,14 @@ import Foundation
 import IOKit
 import PreferencePanes
 
-// FIXME
+
 func getBoolean(_ key: String, _ io_service: io_service_t) -> Bool {
-    let rvalue = IORegistryEntryCreateCFProperty(io_service, key as CFString, kCFAllocatorDefault, 0)
-    if rvalue == nil {
-        return false
+    if let rvalue = IORegistryEntryCreateCFProperty(io_service, key as CFString, kCFAllocatorDefault, 0) {
+        return rvalue.takeRetainedValue() as! Bool
     }
-    return rvalue?.takeRetainedValue() as! Bool
+    return false
 }
 
-// FIXME
 func getNumber(_ key: String, _ io_service: io_service_t) -> Int {
     if let rvalue = IORegistryEntryCreateCFProperty(io_service, key as CFString, kCFAllocatorDefault, 0) {
         return rvalue.takeRetainedValue() as! Int
@@ -33,6 +31,18 @@ func getString(_ key: String, _ io_service: io_service_t) -> String? {
         return rvalue.takeRetainedValue() as! NSString as String
     }
     return nil
+}
+
+func sendBoolean(_ key: String, _ value: Bool, _ io_service: io_service_t) -> kern_return_t {
+    return IORegistryEntrySetCFProperty(io_service, key as CFString, value as CFBoolean)
+}
+
+func sendNumber(_ key: String, _ value: Int, _ io_service: io_service_t) -> kern_return_t {
+    return IORegistryEntrySetCFProperty(io_service, key as CFString, value as CFNumber)
+}
+
+func sendString(_ key: String, _ value: String, _ io_service: io_service_t) -> kern_return_t {
+    return IORegistryEntrySetCFProperty(io_service, key as CFString, value as CFString)
 }
 
 class YogaSMCPane : NSPreferencePane {
@@ -50,12 +60,28 @@ class YogaSMCPane : NSPreferencePane {
     @IBOutlet weak var ThinkViewItem: NSTabViewItem!
     
     @IBOutlet weak var backlightSlider: NSSlider!
-
+    @IBAction func backlightSet(_ sender: NSSlider) {
+        if kIOReturnSuccess != sendNumber("BacklightLevel", backlightSlider.integerValue, io_service) {
+            updateBacklight()
+        }
+    }
+    
     @IBOutlet weak var autoSleepCheck: NSButton!
     @IBOutlet weak var yogaModeCheck: NSButton!
     @IBOutlet weak var indicatorCheck: NSButton!
     @IBOutlet weak var muteCheck: NSButton!
     @IBOutlet weak var micMuteCheck: NSButton!
+
+    @IBAction func autoBacklightSet(_ sender: NSButton) {
+        let val = ((autoSleepCheck.intValue != 0) ? 1 << 0 : 0) +
+                ((yogaModeCheck.intValue != 0) ? 1 << 1 : 0) +
+                ((indicatorCheck.intValue != 0) ? 1 << 2 : 0) +
+                ((muteCheck.intValue != 0) ? 1 << 3 : 0) +
+                ((micMuteCheck.intValue != 0) ? 1 << 3 : 0)
+        if (sendNumber("AutoBacklight", val, io_service) != kIOReturnSuccess) {
+            updateAutoBacklight()
+        }
+    }
 
     override func mainViewDidLoad() {
 //        UserDefaults.standard.set("value", forKey: "testKey")
@@ -73,7 +99,16 @@ class YogaSMCPane : NSPreferencePane {
         return
     }
 
-    func update() {
+    func updateBacklight() {
+        let backlightLevel = getNumber("BacklightLevel", io_service)
+        if (backlightLevel != -1) {
+            backlightSlider.integerValue = backlightLevel
+        } else {
+            backlightSlider.isEnabled = false
+        }
+    }
+
+    func updateAutoBacklight() {
         let autoBacklight = getNumber("AutoBacklight", io_service)
         if (autoBacklight != -1) {
             autoSleepCheck.state = ((autoBacklight & (1 << 0)) != 0) ? NSControl.StateValue.on : NSControl.StateValue.off
@@ -88,19 +123,11 @@ class YogaSMCPane : NSPreferencePane {
             muteCheck.isEnabled = false
             micMuteCheck.isEnabled = false
         }
-
-        let backlightLevel = getNumber("BacklightLevel", io_service)
-        if (backlightLevel != -1) {
-            backlightSlider.integerValue = backlightLevel
-        } else {
-            backlightSlider.isEnabled = false
-        }
-
-        if getBoolean("FnlockMode", io_service) {
-            vECRead.stringValue = "Available"
-        } else {
-            vECRead.stringValue = "Unavailable"
-        }
+    }
+    
+    func update() {
+        updateAutoBacklight()
+        updateBacklight()
 
         if vClass.stringValue == "Idea" {
             updateIdea()
@@ -110,24 +137,24 @@ class YogaSMCPane : NSPreferencePane {
     }
 
     override func awakeFromNib() {
-        io_service = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("YogaSMC"))
+        io_service = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("YogaVPC"))
 
         if (io_service == 0) {
             return
         }
 
         switch IOObjectCopyClass(io_service).takeRetainedValue() as NSString {
-        case "IdeaSMC":
+        case "IdeaVPC":
             vClass.stringValue = "Idea"
             TabView.removeTabViewItem(ThinkViewItem)
             indicatorCheck.isHidden = true
             muteCheck.isHidden = true
             micMuteCheck.isHidden = true
-        case "ThinkSMC":
+        case "ThinkVPC":
             vClass.stringValue = "Think"
             TabView.removeTabViewItem(IdeaViewItem)
-        case "YogaSMC":
-            vClass.stringValue = "Unknown"
+        case "YogaVPC":
+            vClass.stringValue = "Generic"
             TabView.removeTabViewItem(IdeaViewItem)
             TabView.removeTabViewItem(ThinkViewItem)
         default:
@@ -151,6 +178,12 @@ class YogaSMCPane : NSPreferencePane {
 
         if unsupported {
             return
+        }
+
+        if getBoolean("ReadEC", io_service) {
+            vECRead.stringValue = "Available"
+        } else {
+            vECRead.stringValue = "Unavailable"
         }
 
         update()
