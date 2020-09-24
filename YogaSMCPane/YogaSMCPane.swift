23589d4eb24332b6eff0ec8a07894f230ff3a776
Zhen <66577170+zhen-zen@users.noreply.github.com>
2020-09-23 19:52:34 -0700

    Pane: Support ThinkVPC LED control

--- YogaSMCPane/YogaSMCPane.swift
+++ YogaSMCPane/YogaSMCPane.swift
@@ -11,6 +11,55 @@ import Foundation
 import IOKit
 import PreferencePanes
 
+// from https://ffried.codes/2018/01/20/the-internals-of-the-macos-hud/
+@objc enum OSDImage: CLongLong {
+    case kBrightness = 1
+    case brightness2 = 2
+    case kVolume = 3
+    case kMute = 4
+    case volume5 = 5
+    case kEject = 6
+    case brightness7 = 7
+    case brightness8 = 8
+    case kAirportRange = 9
+    case wireless2Forbid = 10
+    case kBright = 11
+    case kBrightOff = 12
+    case kBright13 = 13
+    case kBrightOff14 = 14
+    case Ajar = 15
+    case mute16 = 16
+    case volume17 = 17
+    case empty18 = 18
+    case kRemoteLinkedGeneric = 19
+    case kRemoteSleepGeneric = 20 // will put into sleep
+    case muteForbid = 21
+    case volumeForbid = 22
+    case volume23 = 23
+    case empty24 = 24
+    case kBright25 = 25
+    case kBrightOff26 = 26
+    case backlightonForbid = 27
+    case backlightoffForbid = 28
+    /* and more cases from 1 to 28 (except 18 and 24) */
+}
+
+// from https://github.com/w0lfschild/macOS_headers/blob/master/macOS/CoreServices/OSDUIHelper/1/OSDUIHelperProtocol-Protocol.h
+
+@objc protocol OSDUIHelperProtocol {
+
+    @objc func showFullScreenImage(_ img: OSDImage, onDisplayID: CGDirectDisplayID, priority: CUnsignedInt, msecToAnimate: CUnsignedInt)
+
+    @objc func fadeClassicImageOnDisplay(_ img: CUnsignedInt)
+
+    @objc func showImageAtPath(_ img: NSString, onDisplayID: CGDirectDisplayID, priority: CUnsignedInt, msecUntilFade: CUnsignedInt, withText: NSString)
+
+    @objc func showImage(_ img: OSDImage, onDisplayID: CGDirectDisplayID, priority: CUnsignedInt, msecUntilFade: CUnsignedInt, filledChiclets: CUnsignedInt, totalChiclets: CUnsignedInt, locked: CBool)
+
+    @objc func showImage(_ img: OSDImage, onDisplayID: CGDirectDisplayID, priority: CUnsignedInt, msecUntilFade: CUnsignedInt, withText: NSString)
+
+    @objc func showImage(_ img: OSDImage, onDisplayID: CGDirectDisplayID, priority: CUnsignedInt, msecUntilFade: CUnsignedInt)
+}
 
 func getBoolean(_ key: String, _ io_service: io_service_t) -> Bool {
     guard let rvalue = IORegistryEntryCreateCFProperty(io_service, key as CFString, kCFAllocatorDefault, 0) else {
@@ -40,8 +89,8 @@ func sendBoolean(_ key: String, _ value: Bool, _ io_service: io_service_t) -> ke
     return IORegistryEntrySetCFProperty(io_service, key as CFString, value as CFBoolean)
 }
 
-func sendNumber(_ key: String, _ value: Int, _ io_service: io_service_t) -> kern_return_t {
-    return IORegistryEntrySetCFProperty(io_service, key as CFString, value as CFNumber)
+func sendNumber(_ key: String, _ value: Int, _ io_service: io_service_t) -> Bool {
+    return (kIOReturnSuccess == IORegistryEntrySetCFProperty(io_service, key as CFString, value as CFNumber))
 }
 
 func sendString(_ key: String, _ value: String, _ io_service: io_service_t) -> kern_return_t {
@@ -86,18 +135,49 @@ class YogaSMCPane : NSPreferencePane {
     @IBOutlet weak var vWireless: NSTextField!
     @IBOutlet weak var vWWAN: NSTextField!
     @IBOutlet weak var vGraphics: NSTextField!
-    
+
+    // Think
+
+    @IBOutlet weak var vPowerLEDSlider: NSSlider!
+    @IBOutlet weak var vStandbyLEDSlider: NSSlider!
+    @IBOutlet weak var vThinkDotSlider: NSSliderCell!
+    @IBOutlet weak var vCustomLEDSlider: NSSlider!
+    @IBOutlet weak var vCustomLEDList: NSPopUpButton!
+    @IBAction func vPowerLEDSet(_ sender: NSSlider) {
+        if (!sendNumber("LED", vPowerLEDSlider.integerValue * 0x40 + 0x00, io_service)) {
+            return
+        }
+    }
+    @IBAction func vStandbyLEDSet(_ sender: NSSlider) {
+        if (!sendNumber("LED", vStandbyLEDSlider.integerValue * 0x40 + 0x07, io_service)) {
+            return
+        }
+    }
+    @IBAction func vThinkDotSet(_ sender: NSSlider) {
+        if (!sendNumber("LED", vThinkDotSlider.integerValue * 0x40 + 0x0A, io_service)) {
+            return
+        }
+    }
+    @IBAction func vCustomLEDSet(_ sender: NSSlider) {
+        let value = vCustomLEDSlider.integerValue * 0x40 + vCustomLEDList.indexOfSelectedItem
+        let hex = String(format:"0x%02X", value)
+        let prompt = "LED \(hex)"
+        OSD(prompt)
+        if (!sendNumber("LED", value, io_service)) {
+            return
+        }
+    }
     @IBOutlet weak var TabView: NSTabView!
     @IBOutlet weak var IdeaViewItem: NSTabViewItem!
     @IBOutlet weak var ThinkViewItem: NSTabViewItem!
-    
+
     @IBOutlet weak var backlightSlider: NSSlider!
     @IBAction func backlightSet(_ sender: NSSlider) {
-        if kIOReturnSuccess != sendNumber("BacklightLevel", backlightSlider.integerValue, io_service) {
+        if !sendNumber("BacklightLevel", backlightSlider.integerValue, io_service) {
             updateBacklight()
         }
     }
-    
+
     @IBOutlet weak var autoSleepCheck: NSButton!
     @IBOutlet weak var yogaModeCheck: NSButton!
     @IBOutlet weak var indicatorCheck: NSButton!
@@ -110,7 +190,7 @@ class YogaSMCPane : NSPreferencePane {
                 ((indicatorCheck.state == .on) ? 1 << 2 : 0) +
                 ((muteCheck.state == .on) ? 1 << 3 : 0) +
                 ((micMuteCheck.state == .on) ? 1 << 3 : 0)
-        if (sendNumber("AutoBacklight", val, io_service) != kIOReturnSuccess) {
+        if (!sendNumber("AutoBacklight", val, io_service)) {
             updateAutoBacklight()
         }
     }
@@ -173,7 +253,20 @@ class YogaSMCPane : NSPreferencePane {
                 vGraphics.stringValue = dict.value(forKey: "Graphics") as! NSString as String
             }
         }
-        return
+    }
+
+    func OSD(_ prompt: String) {
+        // from https://ffried.codes/2018/01/20/the-internals-of-the-macos-hud/
+        let conn = NSXPCConnection(machServiceName: "com.apple.OSDUIHelper", options: [])
+        conn.remoteObjectInterface = NSXPCInterface(with: OSDUIHelperProtocol.self)
+        conn.interruptionHandler = { print("Interrupted!") }
+        conn.invalidationHandler = { print("Invalidated!") }
+        conn.resume()
+
+        let target = conn.remoteObjectProxyWithErrorHandler { print("Failed: \($0)") }
+        guard let helper = target as? OSDUIHelperProtocol else { return } //fatalError("Wrong type: \(target)") }
+
+        helper.showImageAtPath("/System/Library/CoreServices/OSDUIHelper.app/Contents/Resources/kBright.pdf", onDisplayID: CGMainDisplayID(), priority: 0x1f4, msecUntilFade: 2000, withText: prompt as NSString)
     }
 
     func updateThink() {
@@ -227,7 +320,7 @@ class YogaSMCPane : NSPreferencePane {
         switch IOObjectCopyClass(io_service).takeRetainedValue() as NSString {
         case "IdeaVPC":
             vClass.stringValue = "Idea"
-            TabView.removeTabViewItem(ThinkViewItem)
+//            TabView.removeTabViewItem(ThinkViewItem)
             indicatorCheck.isHidden = true
             muteCheck.isHidden = true
             micMuteCheck.isHidden = true
