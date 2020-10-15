1a7784624ce18ff561d4ad51a106b6d2454ec4f2
Zhen <66577170+zhen-zen@users.noreply.github.com>
2020-10-15 13:18:02 -0700

    NC: Import ScriptingBridge

--- YogaSMCUtils/ScriptHelper.swift
+++ YogaSMCUtils/ScriptHelper.swift
@@ -8,6 +8,7 @@
 
 import AppKit
 import Foundation
+import ScriptingBridge
 import os.log
 
 let prefpaneAS = """
@@ -18,20 +19,49 @@ let prefpaneAS = """
                  """
 let sleepAS = "tell application \"System Events\" to sleep"
 
+let getMicVolumeAS = "input volume of (get volume settings)"
+
 func prefpaneHelper() {
-    // from https://medium.com/macoclock/everything-you-need-to-do-to-launch-an-applescript-from-appkit-on-macos-catalina-with-swift-1ba82537f7c3
-    if let script = NSAppleScript(source: prefpaneAS) {
-        var error: NSDictionary?
-        script.executeAndReturnError(&error)
-        if error != nil {
-            os_log("Failed to open prefpane", type: .error)
-            let alert = NSAlert()
-            alert.messageText = "Failed to open Preferences"
-            alert.informativeText = "Please install YogaSMCPane"
-            alert.alertStyle = .warning
-            alert.addButton(withTitle: "OK")
-            alert.runModal()
-        }
+    guard let application : SystemPreferencesApplication = SBApplication(bundleIdentifier: "com.apple.systempreferences") else {
+        return
     }
+    let panes = application.panes!().reduce([String: SystemPreferencesPane](), { (dictionary, object) -> [String: SystemPreferencesPane] in
+        let pane = object as! SystemPreferencesPane
+        var dictionary = dictionary
+        if let name = pane.name {
+            dictionary[name] = pane
+        }
+        return dictionary
+    })
+    guard panes.count != 0 else {
+        #if DEBUG
+        showOSD("Please grant access \n to Apple Event")
+        #endif
+        // from https://medium.com/macoclock/everything-you-need-to-do-to-launch-an-applescript-from-appkit-on-macos-catalina-with-swift-1ba82537f7c3
+        if let script = NSAppleScript(source: prefpaneAS) {
+            var error: NSDictionary?
+            script.executeAndReturnError(&error)
+            if error != nil {
+                os_log("Failed to open prefpane", type: .error)
+                let alert = NSAlert()
+                alert.messageText = "Failed to open Preferences"
+                alert.informativeText = "Please install YogaSMCPane"
+                alert.alertStyle = .warning
+                alert.addButton(withTitle: "OK")
+                alert.runModal()
+            }
+        }
+        return
+    }
+    guard let pane = panes["YogaSMCPane"] else {
+        let alert = NSAlert()
+        alert.messageText = "YogaSMCPane not installed"
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
+        return
+    }
+    _ = pane.reveal?()
+    application.activate()
 }
 
