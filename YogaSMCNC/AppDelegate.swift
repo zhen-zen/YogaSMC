4c36ba59ac08c9cfadfc444b32e73cc8e0ec064d
Zhen <66577170+zhen-zen@users.noreply.github.com>
2020-10-20 03:18:47 -0700

    NC: Debug: read HFSP and HFNI

--- YogaSMCNC/AppDelegate.swift
+++ YogaSMCNC/AppDelegate.swift
@@ -34,6 +34,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             } else {
                 os_log("Failed to access EC", type: .error)
             }
+            #if DEBUG
+            outputSize = 1
+            input = 0x2f
+            if kIOReturnSuccess == IOConnectCallMethod(conf.connect, UInt32(kYSMCUCReadEC), &input, 1, nil, 0, nil, nil, &output, &outputSize),
+               outputSize == 1 {
+                appMenu.items[6].title = "HFSP: \(output[0])"
+            }
+            input = 0x83
+            if kIOReturnSuccess == IOConnectCallMethod(conf.connect, UInt32(kYSMCUCReadEC), &input, 1, nil, 0, nil, nil, &output, &outputSize),
+               outputSize == 1 {
+                appMenu.items[7].title = "HFNI: \(output[0])"
+            }
+            #endif
         }
     }
 
@@ -149,6 +162,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     appMenu.insertItem(withTitle: "Version: \(props["YogaSMC,Version"] as? NSString ?? "Unknown")", action: nil, keyEquivalent: "", at: 4)
                     if isThink {
                         appMenu.insertItem(withTitle: "Fan", action: nil, keyEquivalent: "", at: 5)
+                        #if DEBUG
+                        appMenu.insertItem(withTitle: "HFSP", action: nil, keyEquivalent: "", at: 6)
+                        appMenu.insertItem(withTitle: "HFNI", action: nil, keyEquivalent: "", at: 7)
+                        #endif
                         updateThinkFan()
                         updateMuteStatus()
                         NotificationCenter.default.addObserver(self, selector: #selector(updateMuteStatus), name: NSWorkspace.didWakeNotification, object: nil)
