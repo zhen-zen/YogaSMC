f3477aea502a01d5cd704e6d1b554a9028083dd6
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2021-05-02 12:28:02 -0700

    ThinkSMC: separate fan update

--- YogaSMC/YogaSMC/ThinkSMC.hpp
+++ YogaSMC/YogaSMC/ThinkSMC.hpp
@@ -18,6 +18,7 @@ class ThinkSMC : public YogaSMC
     OSDeclareDefaultStructors(ThinkSMC)
 
 private:
+    void updateECFan();
     void addVSMCKey() APPLE_KEXT_OVERRIDE;
     void updateEC() APPLE_KEXT_OVERRIDE;
     virtual inline IOTimerEventSource *initPoller() APPLE_KEXT_OVERRIDE {
