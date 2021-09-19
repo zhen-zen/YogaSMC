0e4fcb4a0a7c8913dfb77c8cd8b4221908cfa1ee
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2021-09-19 01:25:30 -0700

    YogaVPC: fix notification delivery

--- YogaSMC/YogaVPC.cpp
+++ YogaSMC/YogaVPC.cpp
@@ -110,9 +110,6 @@ bool YogaVPC::start(IOService *provider) {
 
     updateAll();
 
-    setProperty(kDeliverNotifications, kOSBooleanTrue);
-    registerService();
-
     if (WMICollection) {
         for (int i=WMICollection->getCount()-1; i >= 0; i--) {
             IOService *wmi = OSDynamicCast(IOService, WMICollection->getObject(i));
@@ -143,6 +140,8 @@ bool YogaVPC::start(IOService *provider) {
         }
     }
 #endif
+    setProperty(kDeliverNotifications, kOSBooleanTrue);
+    registerService();
     return true;
 }
 
@@ -333,7 +332,6 @@ bool YogaVPC::updateClamshell(bool update) {
         return true;
 
     UInt32 state;
-
     if (vpc->evaluateInteger(getClamshellMode, &state) != kIOReturnSuccess) {
         AlwaysLog(updateFailure, clamshellPrompt);
         return false;
